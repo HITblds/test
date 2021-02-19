f62434aa95b73e915ad9977f8380db2dd5da3e70
HITblds <72662662+HITblds@users.noreply.github.com>
2021-02-19 22:21:52 +0800

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,26 +1,7 @@
 #include <iostream>
-#include <iomanip>
-#include <cstring>
 using namespace std;
-int abs(int a)
-{
-	return a<0?-a:a;
-}
-double abs(double a)
-{
-	return a<0?-a:a;
-}
 int main()
 {
-	cout<<abs(-3)<<endl;
-	cout<<abs(-5.65)<<endl;
-	double a = 12.345623;
-	int oldprecision= cout.precision(4);
-	cout<<a<<endl;
-	cout<<oldprecision<<endl;
-	cout<<setprecision(oldprecision)<<a<<endl;
-	char s[128];
-	cin.getline(s,sizeof(s));
-	cout<<s<<endl<<strlen(s);
+	cout<<"hello world!"<<endl;
 	return 0;
 }
